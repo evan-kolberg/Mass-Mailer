c1c8c18c19c34c21690cbf1c56a8896aa06f77d4
python bytes hurt <ekprogamer13@gmail.com>
2021-10-09 08:53:21 -0400

    main.py in now only 6 lines of code!

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import yagmail, csv
 yag = yagmail.SMTP(user='massmailer69@gmail.com', password='************')
 with open('csv.csv') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for name, email, message in reader:
-        yag.send(to=email, subject=f'Hey {name}', contents=message)
-print("Email sent successfully")
+	reader = csv.reader(file); next(reader)
+	for (name, email, message) in reader: yag.send(to=email, subject=f'Hey {name}', contents=message)
+print('Email sent successfully')
